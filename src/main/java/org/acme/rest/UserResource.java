672bb6a52ea7b8eb0d0445ad6aefb6247c6daa54
DelcioFrancisco <delciofrancisco_1@hotmail.com>
2026-02-17 08:40:51 +0000

    git commit "feat: add dto and create user endpoint"

---
+++ src/main/java/org/acme/rest/UserResource.java
@@ -0,0 +1,20 @@
+package org.acme.rest;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.acme.rest.dto.CreateUserRequest;
+
+@Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class UserResource {
+
+    @POST
+    public Response createUser( CreateUserRequest userRequest) {
+        return Response.ok(userRequest).build();
+    }
+}
