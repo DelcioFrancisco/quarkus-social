a247846137109c8d5a3b1f8c55f341f63ca7ae9f
devchicala <delcio.francisco.external@worldline.com>
2026-03-15 12:45:38 +0000

    feat: change simple CreateUserRequest DTO into a Record

--- src/main/java/org/acme/rest/dto/CreateUserRequest.java
+++ src/main/java/org/acme/rest/dto/CreateUserRequest.java
@@ -1,22 +1,3 @@
 package org.acme.rest.dto;
 
-public class CreateUserRequest {
-    private String name;
-    private Integer age;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-}
+public record CreateUserRequest(String name, Integer age) { }
